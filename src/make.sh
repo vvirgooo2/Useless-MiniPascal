4e42ec21d2bb913aa6e5e204a9a3cf48cf67fd74
vvirgooo2 <56679434+vvirgooo2@users.noreply.github.com>
2022-05-21 21:01:37 +0800

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -3,8 +3,4 @@ bison -y -d -o ./parse/yacc.cpp ./parse/grammar.y
 g++ `llvm-config --libs core native --cxxflags --ldflags` -fexceptions ./ast/AST_node.h ./gen/AST_node.cpp ./gen/CodeGen.hpp ./gen/CodeGen.cpp ./astVisualize/dict.cpp ./parse/lex.cpp ./parse/yacc.cpp main.cpp -o parser.out
 rm -f result.ll
 ./parser.out
-<<<<<<< HEAD
 # clang result.ll -o result
-=======
-clang result.ll -o result
->>>>>>> b299ae2cd347548ba097013b244d3e6f60c93210
